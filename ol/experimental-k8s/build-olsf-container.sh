7a35fe8981b54ea2805712b1cf5a7a17902343d1
coin1111 <celticsnow123@gmail.com>
2021-12-28 17:07:15 -0800

    docker

--- ol/experimental-k8s/build-olsf-container.sh
+++ ol/experimental-k8s/build-olsf-container.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-#set -x
+set -x
 set -e
 olsf_container=libra-olsf
 
 if [ "$1" == "-h" ]; then
-	echo "Usage: build-olsf-container.sh [-p <project>]"
+	echo "Usage: build-olsf-container.sh <use_local_bits> [-p <project>]"
 	echo -e "\t Build container with olsf binaries"
 	echo -e "\t -p -publish to dockerhub to <repo>, e.g coin1111/olsf-libra"
+	exit 0
 fi
+use_local_bits="$1"
 
-docker build -t "$olsf_container" -f Dockerfile.olsf .
+if [ "$use_local_bits" == 1 ]; then
+  pushd ../..
+  pwd
+  dockerfile=Dockerfile.local
+  docker build -t "$olsf_container" -f ol/experimental-k8s/"$dockerfile" .
+  popd
+else
+  dockerfile=Dockerfile.olsf
+  docker build -t "$olsf_container" -f "$dockerfile" .
+fi
 ids=$(docker images | grep "$olsf_container" | grep latest | grep -v "gcr" | awk '{print $3}')
 
 for id in "$ids"
@@ -17,12 +28,11 @@ do
 	break
 done
 
-if [ "$1" == "-p" ]; then
-
-    if [ "$2" == "" ]; then
+if [ "$2" == "-p" ]; then
+    if [ "$3" == "" ]; then
 	    repo=coin1111/olsf-libra
     else
-	    repo="$2"
+	    repo="$3"
     fi
     tag="$repo:$id"
     docker tag "$id" "$tag"
