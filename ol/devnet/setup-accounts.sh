7b21a1cf6c85043ea3b13d2eeb68f9df911b80b3
coin1111 <celticsnow123@gmail.com>
2022-01-28 17:39:02 -0800

    accounts

--- ol/devnet/setup-accounts.sh
+++ ol/devnet/setup-accounts.sh
@@ -1,6 +1,8 @@
+pushd ../..
 cargo run -p ol -- --swarm-path=$HOME/swarm_temp --swarm-persona=alice init --source-path $HOME/libra
 
 cargo run -p ol -- --swarm-path=$HOME/swarm_temp --swarm-persona=bob init --source-path $HOME/libra
 
-cp ../fixtures/condif/alice.0L.toml $HOME/swarm_temp/0/0L.toml
-cp ../fixtures/condif/bob.0L.toml $HOME/swarm_temp/1/0L.toml
+cp ol/devnet/fixtures/config/alice.0L.toml $HOME/swarm_temp/0/0L.toml
+cp ol/devnet/fixtures/config/bob.0L.toml $HOME/swarm_temp/1/0L.toml
+popd
