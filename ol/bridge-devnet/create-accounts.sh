00112047684e1b1430b8d2f45a80551a73420406
coin1111 <celticsnow123@gmail.com>
2022-01-29 22:06:05 -0800

    transfer

---
+++ ol/bridge-devnet/create-accounts.sh
@@ -0,0 +1,3 @@
+cargo run -p txs -- --swarm-path=$HOME/swarm_temp --swarm-persona=alice  create-account -a b471d15e523f4fbba50983ed0cb2bdf5770b2c65843b25ca12ca48091fc33cd8  -c 4
+
+cargo run -p txs -- --swarm-path=$HOME/swarm_temp --swarm-persona=bob  create-account -a 1626d2f926949f5f8363df25f053d5258671af7a44f80253f3e141123ff4a7d2  -c 4
