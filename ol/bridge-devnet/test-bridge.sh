8247d041abfe0d017b0a38d0c23b2af872e0048c
coin1111 <celticsnow123@gmail.com>
2022-02-18 21:32:45 -0800

    tests

--- ol/bridge-devnet/test-bridge.sh
+++ ol/bridge-devnet/test-bridge.sh
@@ -16,3 +16,6 @@ cargo run -p txs -- --swarm-path=/home/ruslan/swarm_temp/ --swarm-persona=alice 
 
 # close transfer account on the other chain
 cargo run -p txs -- --swarm-path=/home/ruslan/swarm_temp/ --swarm-persona=alice bridge-close-transfer -e 708B1D23219EB737035CB29A68F0F3A8 -t 1234 -o
+
+# check todd balance
+cargo r -p ol -- --swarm-path=/home/ruslan/swarm_temp/ --swarm-persona=bridge-escrow -a 8671AF7A44F80253F3E141123FF4A7D2 query -b
