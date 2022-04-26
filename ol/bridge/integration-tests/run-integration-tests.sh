53d8c6d614d4fba5bfb7f349ad7bf500f554afaa
coin1111 <celticsnow123@gmail.com>
2022-04-25 18:24:18 -0700

    refactor tests

--- ol/bridge/integration-tests/run-integration-tests.sh
+++ ol/bridge/integration-tests/run-integration-tests.sh
@@ -2,6 +2,5 @@ export SWARM_PATH=/home/ruslan/swarm_temp/
 export SWARM_PERSONA=alice
 export ETH_BRIDGE_ESCROW_CONFIG=/home/ruslan/projects/eth-bridge/.bridge_escrow.config
 export ETH_ACCOUNTS_PATH=/home/ruslan/libra/ol/bridge/accounts-eth
-cargo test --package integration-tests --lib transfer_test::test_transfer_eth_ol -- --exact
-
-cargo test --package integration-tests --lib transfer_test::test_transfer_ol_eth -- --exact 
+cargo test --package integration-tests --lib transfer_test_eth_ol::test_transfer_ol_eth -- --exact
+cargo test --package integration-tests --lib transfer_test_ol_eth::test_transfer_eth_ol -- --exact
