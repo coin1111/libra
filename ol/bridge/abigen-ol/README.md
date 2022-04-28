0a89ce7866ef4c34a542e485c5d6f996a586fb57
coin1111 <celticsnow123@gmail.com>
2022-04-27 19:07:44 -0700

    abigen

---
+++ ol/bridge/abigen-ol/README.md
@@ -0,0 +1,10 @@
+# Generate Abis for Rust Compiler for ETH Contract
+
+```asm
+cargo run --package abigen-ol --bin abigen-ol --  <contract-name> <contract-abi.json> [contract-abi.rs]
+```
+
+## Generate eth bridge mods
+```asm
+./generate-mods.sh
+```
---
+++ ol/bridge/abigen-ol/src/main.rs
@@ -0,0 +1,41 @@
+use ethers::{ utils::Solc};
+use ethers_contract_abigen::Abigen;
+use std::process::exit;
+
+fn main() -> anyhow::Result<()> {
+    let mut args = std::env::args();
+    args.next().unwrap(); // skip program name
+    let help = ||{
+        println!("Usage: abigen-ol <contract-name> <contract-abi.json> [contract-abi.rs]");
+        println!("Generate ETH contract abi for rust compiler");
+        exit(0);
+    };
+    let contract_name =  args.next().unwrap_or_else(help);
+    if &contract_name == "-h" || &contract_name == "--help" {
+        help();
+    }
+    let contract = args.next().unwrap_or_else(help);
+
+
+
+    println!("Generating bindings for {}\n", contract);
+
+    // compile it if needed
+    let abi = if contract.ends_with(".sol") {
+        let contracts = Solc::new(&contract).build_raw()?;
+        contracts.get(&contract_name).unwrap().abi.clone()
+    } else {
+        contract.clone()
+    };
+
+    let bindings = Abigen::new(&contract_name, abi)?.generate()?;
+
+    // print to stdout if no output arg is given
+    if let Some(output_path) = args.next() {
+        bindings.write_to_file(&output_path)?;
+    } else {
+        bindings.write(std::io::stdout())?;
+    }
+
+    Ok(())
+}
