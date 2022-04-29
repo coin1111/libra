5442186c116eaecaeb1a895abd7f7897eab3996f
coin1111 <celticsnow123@gmail.com>
2022-04-28 20:15:10 -0700

    cleanup

--- ol/bridge/setup-accounts.sh
+++ ol/bridge/setup-accounts.sh
@@ -1,9 +1,20 @@
 set -e
+
+
 pushd ../..
 cargo run -p ol -- --swarm-path=$HOME/swarm_temp --swarm-persona=alice init --source-path $HOME/libra
 
 cargo run -p ol -- --swarm-path=$HOME/swarm_temp --swarm-persona=bob init --source-path $HOME/libra
 
-cp ol/fixtures/configs/alice.0L.toml $HOME/swarm_temp/0/0L.toml
-cp ol/fixtures/configs/bob.0L.toml $HOME/swarm_temp/1/0L.toml
+
+# fix $HOME dir inside fixture files
+i=0
+for v in ol/bridge/fixtures/configs/alice.0L.toml ol/bridge/fixtures/configs/bob.0L.toml
+do 
+    b=$HOME;c=$(echo $b | sed -e s/\\//\\\\\\//g);echo $c
+    a='sed -e s/\$HOME/"$c"/ "$v"'
+    eval $a > $HOME/swarm_temp/"$i"/0L.toml
+    i=$((i+1))
+done
+
 popd
