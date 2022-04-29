bf2cc13574c71cc853c9b62b9f67a0fc618e3c56
coin1111 <celticsnow123@gmail.com>
2022-04-28 19:30:47 -0700

    cleanup

--- ol/bridge/agent/run-agent.sh
+++ ol/bridge/agent/run-agent.sh
@@ -1,3 +1,3 @@
-export ETH_BRIDGE_ESCROW_CONFIG=/home/ruslan/projects/eth-bridge/.bridge_escrow.config
-export ETH_BRIDGE_ESCROW_ACCOUNT=/home/ruslan/projects/eth-bridge/accounts/alice.txt
-cargo run -p bridge-agent -- --swarm-path=/home/ruslan/swarm_temp/ --swarm-persona=alice  -a 708B1D23219EB737035CB29A68F0F3A8 agent 
+export ETH_BRIDGE_ESCROW_CONFIG=$HOME/projects/eth-bridge/.bridge_escrow.config
+export ETH_BRIDGE_ESCROW_ACCOUNT=$HOME/projects/eth-bridge/accounts/alice.txt
+cargo run -p bridge-agent -- --swarm-path=$HOME/swarm_temp/ --swarm-persona=alice  -a 708B1D23219EB737035CB29A68F0F3A8 agent 
--- ol/bridge/get-escrow-state.sh
+++ ol/bridge/get-escrow-state.sh
@@ -2,4 +2,4 @@ if [ "$1" == "" ]; then
     echo "Usage: get-escrow-state.sh [locked|unlocked]"
     exit 1
 fi
-cargo run -p ol -- --swarm-path=/home/ruslan/swarm_temp/ --swarm-persona=bridge-escrow  -a 708B1D23219EB737035CB29A68F0F3A8 query --move-state --move-module BridgeEscrow --move-struct EscrowState --move-value "$1"
+cargo run -p ol -- --swarm-path=$HOME/swarm_temp/ --swarm-persona=bridge-escrow  -a 708B1D23219EB737035CB29A68F0F3A8 query --move-state --move-module BridgeEscrow --move-struct EscrowState --move-value "$1"
